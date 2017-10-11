aae3447585b4c5a56f2dd809f5f52af2b5c70925
Richard-Howard-Toronto <rh_gk@rogers.com>
2017-10-10 21:17:07 -0400

    using the array method

--- lesson_21_collections.rb
+++ lesson_21_collections.rb
@@ -3,6 +3,7 @@
 digits = ['1','2','3','4','5','6','7','8','9']
 english = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 french = ['un','deux','trois','quatre','cinq','six','sept','huit','neuf']
+spanish = ['uno','dos','tres','cuatro','cinco','seis','siete','ocho','nueve']
 
 # {
 #   1: {french: 'un', english: 'one'},
@@ -20,19 +21,27 @@ p h6values = h6.values
 results = [{a:1, b:2},{a:3, b:4}]
 results.each {|h| h[:c]="newvalue" unless h.include? :c}
 
+require 'pp'
 
-# examples below
+main_hash = {}
 
-# members     = ["Matt Anderson", "Justin Biltonen", "Jordan Luff", "Jeremy London"]
-# instruments = ["guitar, vocals", "guitar", "bass", "drums"]
-# p Hash[members.zip(instruments.map{|i| i.include?(',') ? i.split(',') : i})]
-#
-# @sample_array = ["one", "Two", "Three"]
-# @timesheet_id_array = ["96", "97", "98"]
-# p h1 = Hash[@sample_array.zip(@timesheet_id_array)]
-#
-# keys = [1, 2, 3]
-# values = ['a', 'b', 'c']
-# p h2 = Hash[keys.zip(values)]
-# p h3 = Hash[english.zip(french)]
-# p h4 = Hash[h3.zip(digits)]
+(1..9).each do |index|
+  main_hash[index] = {english: "#{english[index-1]}", french: "#{french[index-1]}", spanish: "#{spanish[index-1]}"}
+end
+
+pp main_hash
+
+
+members     = ["Matt Anderson", "Justin Biltonen", "Jordan Luff", "Jeremy London"]
+instruments = ["guitar, vocals", "guitar", "bass", "drums"]
+p Hash[members.zip(instruments.map{|i| i.include?(',') ? i.split(',') : i})]
+
+@sample_array = ["one", "Two", "Three"]
+@timesheet_id_array = ["96", "97", "98"]
+p h1 = Hash[@sample_array.zip(@timesheet_id_array)]
+
+keys = [1, 2, 3]
+values = ['a', 'b', 'c']
+p h2 = Hash[keys.zip(values)]
+p h3 = Hash[english.zip(french)]
+p h4 = Hash[h3.zip(digits)]
